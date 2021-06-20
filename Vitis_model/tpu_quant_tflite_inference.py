a7d9aee5bf4903ed05e03ef73d8680f7e64f43f3
ekellim <56914258+ekellim@users.noreply.github.com>
2021-06-20 21:07:43 +0200

    Update tpu_quant_tflite_inference.py

--- Vitis_model/tpu_quant_tflite_inference.py
+++ Vitis_model/tpu_quant_tflite_inference.py
@@ -41,7 +41,6 @@ for model_nr in np.arange(1):
     times.append(inference_time * 1000)
     output_data = interpreter.get_tensor(output_details[0]['index'])
     y_pred[i]=np.argmax(output_data)
-    print(np.argmax(output_data), np.argmax(y_test[i]))
     if np.argmax(output_data) == np.argmax(y_test[i]):
         acc += 1
   Y_test = np.argmax(y_test, axis=1) # Convert one-hot to index
