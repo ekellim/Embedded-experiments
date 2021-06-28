4577213c051e638051735eb0031b1ce1743584e0
ekellim <56914258+ekellim@users.noreply.github.com>
2021-06-28 16:33:22 +0200

    Update loadDataset.py

--- Vitis_model/loadDataset.py
+++ Vitis_model/loadDataset.py
@@ -3,14 +3,14 @@
 import numpy as np
 import pandas as pd
 import os.path
-from skimage import io
+import cv2
 from tensorflow.keras.utils import to_categorical
 from config import *
 
 
 def loadImage(filePath):
     # open image as BGR
-    image = io.imread(filePath, as_gray=True)
+    image = cv2.imread(filePath, cv2.IMREAD_GRAYSCALE)
 
     # normalize
     image = image/255.0
